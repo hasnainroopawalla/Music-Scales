d44657db3b96ed7e06ea309d39d4cacb7e778bf1
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2019-04-11 20:45:33 +0530

    Visual

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ from flask import Flask,request,render_template,redirect,url_for
 
 app = Flask(__name__)
 
+@app.route("/scalefinder")
+def scalefind():
+    return render_template('scalefinder.html')
+
 @app.route("/")
 def firstpage():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=9000, debug=True)
