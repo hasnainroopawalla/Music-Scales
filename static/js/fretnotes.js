12eae7f92700d7cbad88f8bba035298a9460f761
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2019-04-13 16:10:20 +0530

    Fretboard Visual

---
+++ static/js/fretnotes.js
@@ -0,0 +1,355 @@
+var CNotes = [{
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 8
+    }]
+},/*  {
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 20
+    }]
+}, */{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 1
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 13
+    }]
+},{
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 5
+    }]
+}, {
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 17
+    }]
+}, {
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 10
+    }]
+},/* {
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 22
+    }]
+}, */{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 3
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 15
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 8
+    }]
+}/* ,{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 20
+    }]
+} */];
+
+var CsNotes = [{
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 9
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 2
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 14
+    }]
+},{
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 6
+    }]
+}, {
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 18
+    }]
+}, {
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 11
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 4
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 16
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 9
+    }]
+},];
+
+
+var DNotes = [{
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 10
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 3
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 15
+    }]
+},{
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 7
+    }]
+},{
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 0
+    }]
+}, {
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 12
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 5
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 17
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 10
+    }]
+}];
+
+
+var DsNotes = [{
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 11
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 4
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 16
+    }]
+},{
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 8
+    }]
+},{
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 1
+    }]
+}, {
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 13
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 6
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 18
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 11
+    }]
+}];
+
+
+var ENotes = [{
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 0
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 4
+    }, notes: [{
+        fret: 12
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 5
+    }]
+},{
+    string: {
+        letter: "B",
+        octave: 3
+    }, notes: [{
+        fret: 17
+    }]
+},{
+    string: {
+        letter: "G",
+        octave: 3
+    }, notes: [{
+        fret: 9
+    }]
+},{
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 2
+    }]
+}, {
+    string: {
+        letter: "D",
+        octave: 3
+    }, notes: [{
+        fret: 14
+    }]
+},{
+    string: {
+        letter: "A",
+        octave: 2
+    }, notes: [{
+        fret: 7
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 0
+    }]
+},{
+    string: {
+        letter: "E",
+        octave: 2
+    }, notes: [{
+        fret: 12
+    }]
+}];
